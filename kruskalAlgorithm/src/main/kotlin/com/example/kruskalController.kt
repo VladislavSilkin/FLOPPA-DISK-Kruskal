d6112f5db0e89fd547c8f2183bffab06fc6a3cd7
neyther <ser.atamanoff@yandex.ru>
2021-07-12 20:32:39 +0300

    Interface update

--- kruskalAlgorithm/src/main/kotlin/com/example/kruskalController.kt
+++ kruskalAlgorithm/src/main/kotlin/com/example/kruskalController.kt
@@ -1,24 +1,68 @@
 package com.example
 
-import com.example.view.MainView
 import tornadofx.Controller
-import tornadofx.FileChooserMode
-import tornadofx.View
-import tornadofx.anchorpane
 import java.io.File
 
 class KruskalController : Controller(){
-    val workGraph = KruskalWork()
-    val mainView: MainView by inject()
+    private val workGraph = KruskalWork()
     fun callCreateGraph(path: List<File>) : KruskalWork{
-        val fileGraph: File = File(path[0].toString())
+        val fileGraph = File(path[0].toString())
+        val rightLine = ArrayList<String>()
+
         for(item in fileGraph.readLines()[0]){
             if(!item.isLetter() && item != ' ')
                 throw Exception("Wrong vertexName")
         }
+
+       // for(item in fileGraph.readLines())
+        //    rightLine.add(getStringWithoutSpaces(item))
         return workGraph.createGraph(fileGraph.readLines())
     }
 
-    fun callGetGraph(){
+    fun callSaveGraph(saveFile: File){
+        var writeString = String()
+        for(item in workGraph.vectorOfVertex){
+            if(item == workGraph.vectorOfVertex[workGraph.vectorOfVertex.size-1])
+                writeString += "$item\n"
+            else
+                writeString += "$item "
+        }
+
+        for(item in workGraph.printGraph){
+            writeString += "${item.v1} ${item.v2} ${item.weight.toString()}\n"
+        }
+
+        saveFile.writeText(writeString)
+    }
+
+    fun callGetGraph() : ArrayList<Edge>{
+        return workGraph.printGraph
+    }
+
+    fun callGetVertexes() : ArrayList<String>{
+        return workGraph.vectorOfVertex
+    }
+
+    fun callDoKruskal(){
+        workGraph.doKruskal()
+    }
+
+    fun callClearGraph(){
+        workGraph.clearGraph()
+    }
+
+    fun callUpdatePrintData(){
+        workGraph.modifyGraph()
+    }
+
+    fun getStringWithoutSpaces(changeString: String) : String{
+        var outputString = String()
+        for(item in changeString) {
+            outputString += if (item != changeString[changeString.length - 1])
+                "$item "
+            else
+                "$item"
+        }
+        return outputString
     }
 }
